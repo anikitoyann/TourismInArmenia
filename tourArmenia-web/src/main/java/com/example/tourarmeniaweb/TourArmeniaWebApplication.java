9049d35cc1879f00e3549f37baac2bd5e4e84048
StepanyanLiana <88manelian77@gmail.com>
2023-06-26 12:15:08 +0400

    rest security

---
+++ tourArmenia-web/src/main/java/com/example/tourarmeniaweb/TourArmeniaWebApplication.java
@@ -0,0 +1,26 @@
+package com.example.tourarmeniaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example.tourarmeniaweb", "com.example.tourarmeniacommon"})
+@EntityScan("com.example.tourarmeniacommon.entity")
+@EnableJpaRepositories(basePackages = "com.example.tourarmeniacommon.repository")
+public class TourArmeniaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourArmeniaWebApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
+}
