d92e1d74845fb65a00bda95966775784a619435a
StepanyanLiana <88manelian77@gmail.com>
2023-07-07 15:31:04 +0400

    filter,sort,pagination

--- tourArmenia-rest/src/main/java/com/example/tourarmeniarest/endpoint/ItemEndpoint.java
+++ tourArmenia-rest/src/main/java/com/example/tourarmeniarest/endpoint/ItemEndpoint.java
@@ -1,24 +1,14 @@
 package com.example.tourarmeniarest.endpoint;
 
-import com.example.tourarmeniacommon.dto.CreateItemRequestDto;
 import com.example.tourarmeniacommon.dto.ItemDto;
+import com.example.tourarmeniacommon.dto.ItemSearchDto;
 import com.example.tourarmeniacommon.entity.Item;
-import com.example.tourarmeniacommon.entity.Region;
-import com.example.tourarmeniacommon.mapper.ItemMapper;
 import com.example.tourarmeniacommon.service.ItemService;
-import com.example.tourarmeniacommon.service.RegionService;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,12 +17,6 @@ import java.util.Optional;
 @RequestMapping("/item")
 public class ItemEndpoint {
     private final ItemService itemService;
-    private final RegionService regionService;
-    private final ItemMapper itemMapper;
-    @Value("${upload.image.path}")
-    private String uploadPath;
-    @Value("${site.url}")
-    private String siteUrl;
 
     @GetMapping("/{id}")
     public ResponseEntity<Item> singleItem(@PathVariable("id") int id) {
@@ -42,5 +26,11 @@ public class ItemEndpoint {
             return ResponseEntity.ok(item);
         }
         return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+    @PostMapping("/search")
+    public ResponseEntity<List<ItemDto>> getAll(@RequestParam(value = "size", defaultValue = "20") int size,
+                                                @RequestParam(value = "page", defaultValue = "0") int page,
+                                                @RequestBody ItemSearchDto itemSearchDto) {
+        return ResponseEntity.ok(itemService.search(page, size, itemSearchDto));
     }
 }
