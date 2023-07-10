75853dcd2f339beea399a9d4d6993c9aed9b14cf
StepanyanLiana <88manelian77@gmail.com>
2023-07-10 11:51:14 +0400

    rollback issues after merge conflicts

---
+++ tourArmenia-common/src/main/java/com/example/tourarmeniacommon/dto/RegionDto.java
@@ -0,0 +1,12 @@
+package com.example.tourarmeniacommon.dto;
+
+import lombok.Data;
+
+@Data
+public class RegionDto {
+
+    private int id;
+    private String name;
+    private String regionalCenter;
+
+}
