e2585f1cdb7b6621c297f6196ddd209d7b8b57f1
AnahitN <tanahit360@gmail.com>
2023-07-29 01:14:55 +0400

    corrected services

--- tourArmenia-common/src/main/java/com/example/tourarmeniacommon/service/serviceImpl/MailServiceImpl.java
+++ tourArmenia-common/src/main/java/com/example/tourarmeniacommon/service/serviceImpl/MailServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.tourarmeniacommon.service.serviceImpl;
 
 import com.example.tourarmeniacommon.service.MailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -10,8 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MailServiceImpl implements MailService {
-
-    private final MailSender mailSender;
 
     @Async
     public void sendMail(String to, String subject, String text) {
